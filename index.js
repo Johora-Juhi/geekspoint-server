11304f46484b9d60922c5c3bd2b1cbb442152a0d
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-10-27 18:32:32 +0600

    price added

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const courses = require('./data/coursesName.json')
 const courseDetails = require('./data/courseDetails.json')
 
 app.get('/', (req, res) => {
-    res.send('News API running');
+    res.send('Course API running');
 });
 
 app.get('/courses', (req, res) => {
-    res.send(courses);
+    res.send(courseDetails);
 })
 
 app.get('/course', (req, res) => {
     res.send(courseDetails);
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourse = courseDetails.find(n => n._id === id);
+    res.send(selectCourse);
+})
+app.listen(port, () => {
+    console.log('Newsportal running om this port', port);
 })
